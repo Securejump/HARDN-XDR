f3e40fcc019f4306bf186c17d91b2576979590ca
Samuel P. <samsam70000@gmail.com>
2025-06-23 10:30:17 -0400

    moved: secure networking module

--- src/setup/hardn-main.sh
+++ src/setup/hardn-main.sh
@@ -241,30 +241,11 @@ setup_security(){
 	source ./modules/shared_mem.sh
 	source ./modules/coredumps.sh
 	source ./modules/auto_updates.sh
+	source ./modules/secure_net.sh
+
 
     
     HARDN_STATUS "info" "Setting up security tools and configurations..."
-    
-
-    ########################### Secure network parameters
-    HARDN_STATUS "info" "Configuring secure network parameters..."
-    {
-        echo "net.ipv4.ip_forward = 0"
-        echo "net.ipv4.conf.all.send_redirects = 0"
-        echo "net.ipv4.conf.default.send_redirects = 0"
-        echo "net.ipv4.conf.all.accept_redirects = 0"
-        echo "net.ipv4.conf.default.accept_redirects = 0"
-        echo "net.ipv4.conf.all.secure_redirects = 0"
-        echo "net.ipv4.conf.default.secure_redirects = 0"
-        echo "net.ipv4.conf.all.log_martians = 1"
-        echo "net.ipv4.conf.default.log_martians = 1"
-        echo "net.ipv4.icmp_echo_ignore_broadcasts = 1"
-        echo "net.ipv4.icmp_ignore_bogus_error_responses = 1"
-        echo "net.ipv4.tcp_syncookies = 1"
-        echo "net.ipv6.conf.all.disable_ipv6 = 1"
-        echo "net.ipv6.conf.default.disable_ipv6 = 1"
-    } >> /etc/sysctl.conf
-
     
     #################################### rkhunter
     HARDN_STATUS "info" "Configuring rkhunter..."
