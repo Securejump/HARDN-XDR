c661175c19b77225e6c16279ed9d195f730683fe
Samuel P. <samsam70000@gmail.com>
2025-06-23 09:58:21 -0400

    moved: deleted files module

--- src/setup/hardn-main.sh
+++ src/setup/hardn-main.sh
@@ -233,21 +233,6 @@ setup_security(){
     # global variables CURRENT_DEBIAN_VERSION_ID and CURRENT_DEBIAN_CODENAME are available.
     HARDN_STATUS "pass" "Using detected system: Debian ${CURRENT_DEBIAN_VERSION_ID} (${CURRENT_DEBIAN_CODENAME}) for security setup."
     
- ####################### DELETED FILES
-    HARDN_STATUS "info" "Checking for deleted files in use..."
-    if command -v lsof >/dev/null 2>&1; then
-        deleted_files=$(lsof +L1 | awk '{print $9}' | grep -v '^$')
-        if [[ -n "$deleted_files" ]]; then
-            HARDN_STATUS "warning" "Found deleted files in use:"
-            echo "$deleted_files"
-            HARDN_STATUS "warning" "Please consider rebooting the system to release these files."
-        else
-            HARDN_STATUS "pass" "No deleted files in use found."
-        fi
-    else
-        HARDN_STATUS "error" "lsof command not found. Cannot check for deleted files in use."
-    fi
-    
 ################################## ntp daemon
     HARDN_STATUS "info" "Setting up NTP daemon..."
 
