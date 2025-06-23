a844a900f43d7895e6af789109a4d27b817ca3f6
Samuel P. <samsam70000@gmail.com>
2025-06-23 10:29:13 -0400

    moved: automatic updates module

--- src/setup/hardn-main.sh
+++ src/setup/hardn-main.sh
@@ -240,50 +240,12 @@ setup_security(){
 	source ./modules/file_perms.sh
 	source ./modules/shared_mem.sh
 	source ./modules/coredumps.sh
+	source ./modules/auto_updates.sh
+
     
     HARDN_STATUS "info" "Setting up security tools and configurations..."
     
 
-    ############################### automatic security updates
-    HARDN_STATUS "info" "Configuring automatic security updates for Debian-based systems..."
-
-    case "${ID}" in # Use ${ID} from /etc/os-release
-        "debian")
-            cat > /etc/apt/apt.conf.d/50unattended-upgrades << EOF
-Unattended-Upgrade::Allowed-Origins {
-    "${ID}:${CURRENT_DEBIAN_CODENAME}-security";
-    "${ID}:${CURRENT_DEBIAN_CODENAME}-updates";
-};
-Unattended-Upgrade::Package-Blacklist {
-    // Add any packages you want to exclude from automatic updates
-};
-Unattended-Upgrade::AutoFixInterruptedDpkg "true";
-Unattended-Upgrade::MinimalSteps "true";
-Unattended-Upgrade::Remove-Unused-Kernel-Packages "true";
-Unattended-Upgrade::Remove-New-Unused-Dependencies "true";
-Unattended-Upgrade::Remove-Unused-Dependencies "false";
-Unattended-Upgrade::Automatic-Reboot "false";
-EOF
-            ;;
-        "ubuntu")
-            cat > /etc/apt/apt.conf.d/50unattended-upgrades << EOF
-Unattended-Upgrade::Allowed-Origins {
-    "${ID}:${CURRENT_DEBIAN_CODENAME}-security";
-    "${ID}ESMApps:${CURRENT_DEBIAN_CODENAME}-apps-security";
-    "${ID}ESM:${CURRENT_DEBIAN_CODENAME}-infra-security";
-};
-EOF
-            ;;
-        *)
-            # Generic Debian-based fallback
-            cat > /etc/apt/apt.conf.d/50unattended-upgrades << EOF
-Unattended-Upgrade::Allowed-Origins {
-    "${ID}:${CURRENT_DEBIAN_CODENAME}-security";
-};
-EOF
-            ;;
-    esac
-    
     ########################### Secure network parameters
     HARDN_STATUS "info" "Configuring secure network parameters..."
     {
