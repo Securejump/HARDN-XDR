a7176e53fe70b65eda051090807ddfa066a5fbe2
Samuel P. <samsam70000@gmail.com>
2025-06-23 23:24:28 -0400

    deleted: retrieve_repo and check_git

--- install.sh
+++ install.sh
@@ -11,17 +11,6 @@ update_system() {
         apt update && apt upgrade -y
 }
 
-
-check_git() {
-        printf "\033[1;31m[+] Checking if git is installed, and installing it if not..\033[0m\n"
-        if [ -x "$(command -v git)" ]; then
-          printf "\033[1;32m[+] Git is installed.\033[0m\n"
-        else
-           sudo apt install git -y
-           printf "\033[1;32m[+] Git is now installed.\033[0m\n""]"
-         fi
-}
-
 # Git clone the repo, then cd into the repo and run the script hardn-main.sh
 run() {
         cd HARDN-XDR/src/setup && chmod +x hardn-main.sh && sudo ./hardn-main.sh
@@ -30,8 +19,7 @@ run() {
 main() {
         check_root
         update_system
-        check_git
-        retrieve_repo
+        run
 }
 
 main
